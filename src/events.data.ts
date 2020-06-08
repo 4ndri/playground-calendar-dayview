4c0416f0771c34c042581319d7abe85154daeb7f
Andri Capol <andri.capol@iseag.ch>
2020-06-08 23:01:33 +0000

    performance test

--- src/events.data.ts
+++ src/events.data.ts
@@ -1,12 +1,21 @@
 import { IEvent, IExtendedEvent } from './event.model';
 import data from './data.json';
-import { parseISO } from 'date-fns';
-export const items = (data as unknown) as IEvent[];
 
-const extendedEventMapper = (e: IEvent) =>
-  ({
+import datatest from './data.extreme2.json';
+import { toDate } from 'date-fns-tz';
+
+console.log(datatest.length);
+console.log(data.length);
+
+export const items = ((datatest || data) as unknown) as IEvent[];
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const extendedEventMapper = (e: IEvent, index: number) => {
+  const start = toDate(e.start?.dateTime ?? '', { timeZone: 'UTC' });
+  const end = toDate(e.end?.dateTime ?? '', { timeZone: 'UTC' });
+  return {
     data: e,
-    id: e.id,
+    id: index.toString(),
     after: [],
     before: [],
     bottom: 0,
@@ -15,9 +24,10 @@ const extendedEventMapper = (e: IEvent) =>
     right: 0,
     col: 0,
     groupIndex: 0,
-    startDate: parseISO(e.start?.dateTime ?? ''),
-    endDate: parseISO(e.end?.dateTime ?? ''),
-  } as IExtendedEvent);
+    startDate: start,
+    endDate: end,
+  } as IExtendedEvent;
+};
 
 export const getExtended = () => {
   return items.map(extendedEventMapper);
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Column } from './column';
+import Column from './column';
+import ColumnPrim from './column-primitive';
 import { getExtended } from './events.data';
+
+const colsStyle = css`
+  display: flex;
+  flex-direction: row;
+`;
+
 const App: React.FC = () => {
   return (
-    <div>
-      <Column events={getExtended()} />
+    <div css={colsStyle}>
+      <div><Column events={getExtended()} /></div>
+      <div><ColumnPrim events={getExtended()} /></div>
+      <div><Column events={getExtended()} /></div>
+      <div><ColumnPrim events={getExtended()} /></div>
+      <div><Column events={getExtended()} /></div>
     </div>
   );
 };
